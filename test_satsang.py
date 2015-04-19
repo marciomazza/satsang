2a9e714f4997a39852dd56b09c1c49d1b41cebb3
Marcio Mazza <marciomazza@gmail.com>
2015-04-19 13:32:27 -0300

    Refactor tests

--- test_satsang.py
+++ test_satsang.py
@@ -3,8 +3,7 @@ from mock import patch
 
 from satsang import SpeechSegment
 
-
-@pytest.mark.parametrize('silent_ranges, expected', [
+silent_ranges_and_splits = [
     [[(20, 40), (70, 80)],
      [(0, 0, 20 + 2), (20 + 2, 40 - 2, 70 + 2), (70 + 2, 80 - 2, 100)]],
 
@@ -14,20 +13,7 @@ from satsang import SpeechSegment
     [[(0, 100)], []],
 
     [[], [(0, 0, 100)]],
-])
-def test_split_ranges(silent_ranges, expected):
-    with patch('satsang.detect_silence') as stub:
-        stub.return_value = silent_ranges
-
-        seg = SpeechSegment(range(100))
-        assert seg.split_ranges(10, None, 2) == expected
-
-        seg.split(silence_margin=2)
-        assert [s.speech_start for s in seg.children] == [ss - s for s, ss, e in expected]
-
-        children_audio_chain = (x for child in seg.children
-                                for x in child.audio_segment)
-        assert all(x == y for x, y in zip(seg.audio_segment, children_audio_chain))
+]
 
 
 def test_margin_too_big():
@@ -35,3 +21,19 @@ def test_margin_too_big():
     with pytest.raises(AssertionError) as excinfo:
         seg.split_ranges(10, None, 5)
     assert 'is too big for' in str(excinfo.value)
+
+
+@pytest.mark.parametrize('silent_ranges, splits', silent_ranges_and_splits)
+def test_split_ranges(silent_ranges, splits):
+    with patch('satsang.detect_silence') as detect_silence:
+        detect_silence.return_value = silent_ranges
+
+        seg = SpeechSegment(range(100))
+        assert seg.split_ranges(10, None, 2) == splits
+
+        seg.split(silence_margin=2)
+        assert [s.speech_start for s in seg.children] == [ss - s for s, ss, e in splits]
+
+        children_audio_chain = (x for child in seg.children
+                                for x in child.audio_segment)
+        assert all(x == y for x, y in zip(seg.audio_segment, children_audio_chain))
